b5538de3ec1ecdc80455484a067913db89b63134
Deba9t6jj <debatalukder98@gmail.com>
2025-07-19 19:13:59 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import Leaderboard from './components/Leaderboard';
+
+export default function Home() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Sonic Kaito Leaderboard</h1>
+      <Leaderboard />
+    </div>
+  );
+}
